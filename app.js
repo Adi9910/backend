e2ce5cc55beaecd0fd756c9ec098cb64dc5263ed
Adi9910 <114596621+Adi9910@users.noreply.github.com>
2022-10-08 09:58:51 +0530

    mysql remove

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 const express = require("express");
-
+const mongoose = require("mongoose");
 const app = express();
 
+const db =
+  "mongodb+srv://adibaba:NOIoff09@cluster0.9nevc53.mongodb.net/user?retryWrites=true&w=majority";
+
+mongoose.connect(db)
+  .then(() => {
+    console.log("connect success");
+  })
+  .catch((err) => {
+    console.log("err");
+  });
 //middleware
 
 const middleware = (req, res, next) => {
-  console.log(`hello`);
+  console.log(`hello`); 
   next();
 };
 
