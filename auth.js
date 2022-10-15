538aa25f89ddaaff7d4434132cb3c151ada5ae49
Adi9910 <114596621+Adi9910@users.noreply.github.com>
2022-10-15 11:53:28 +0530

    done

--- auth.js
+++ auth.js
@@ -49,7 +49,7 @@ router.post("/signin", async (req, res) => {
     const token = await userLogin.generateAuthToken();
     console.log(token)
     res.cookie("jwtoken", token,{
-      expires: new Date(date.now()+ 25892000000),
+      expires: new Date(Date.now()+ 25892000000),
       httpOnly:true
     })
 
